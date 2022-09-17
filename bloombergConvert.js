96feb3b265f73a0cd6d5e0fbc77541d239d5a7ce
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2022-09-17 10:14:05 +0200

    File name pre-formating for Bloomberg.

---
+++ bloombergConvert.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+
+
+// Starting from a hardcoded directory
+const directory = "/mnt/c/Retrospectiva/ZParaRenombrar";
+
+// const directory = process.argv[2];
+console.log(directory)
+// 
+
+fs.readdir(directory, (error, files) => {
+  if (error) {
+    throw error;
+  }
+
+  for (let file of files) {
+
+    let pdfFile = directory + '/' + file;
+
+    const firstUnderscore = file.indexOf('-');
+    const name = [...file].slice(0, firstUnderscore - 1).join('');
+    const composed = " - Bloomberg - " + name;
+
+    let renameFile = directory + '/' + composed + ".pdf";
+
+    fs.rename(pdfFile, renameFile, function (error) {
+      if (error) {
+        console.log("ERROR: " + error);
+      }
+
+      console.log(renameFile + " : File name successfully changed.");
+    });
+  }
+});
