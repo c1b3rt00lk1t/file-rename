abb75ce032bd4f37e110e4827ef5d78260bc9948
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2022-09-17 10:13:44 +0200

    File name pre-formating for Expansión.

---
+++ expansionConvert.js
@@ -0,0 +1,37 @@
+const fs = require("fs");
+
+
+// Starting from a hardcoded directory
+const directory = "/mnt/c/Retrospectiva/ZParaRenombrar";
+
+// const directory = process.argv[2];
+console.log(directory)
+
+
+fs.readdir(directory, (error, files) => {
+  if (error) {
+    throw error;
+  }
+
+  for (let file of files) {
+
+    let pdfFile = directory + '/' + file;
+
+    const firstUnderscore = file.indexOf('_');
+    const day = [...file].slice(firstUnderscore - 2, firstUnderscore).join('');
+    const month = [...file].slice(firstUnderscore + 1, firstUnderscore + 3).join('');
+    const year = [...file].slice(firstUnderscore + 4 , firstUnderscore + 8).join('');
+    const page = [...file].slice(file.length - 6 , file.length - 4).join('');
+
+
+    let renameFile = directory + '/' + year + month + day + " - Expansión - " + page + ".pdf";
+
+    fs.rename(pdfFile, renameFile, function (error) {
+      if (error) {
+        console.log("ERROR: " + error);
+      }
+
+      console.log(renameFile + " : File name successfully changed.");
+    });
+  }
+});
