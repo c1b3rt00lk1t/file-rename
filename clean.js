c43bb929f4a896c227ce92dc397a671d6d41ec80
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2022-09-17 10:14:40 +0200

    Cleansing of rare characters in the file names.

---
+++ clean.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+
+
+// Starting from a hardcoded directory
+const directory = "/mnt/c/Retrospectiva/ZParaRenombrar";
+
+// const directory = process.argv[2];
+console.log(directory)
+// 
+
+fs.readdir(directory, (error, files) => {
+  if (error) {
+    throw error;
+  }
+
+  for (let file of files) {
+
+    let pdfFile = directory + '/' + file;
+
+    const clean = file.replace(/[“”‘’&\/\\#,+()$~%'":*?<>{}]/g,'_');
+
+    let renameFile = directory + '/' + clean ;
+
+    fs.rename(pdfFile, renameFile, function (error) {
+      if (error) {
+        console.log("ERROR: " + error);
+      }
+
+      console.log(renameFile + " : File name successfully changed.");
+    });
+  }
+});
