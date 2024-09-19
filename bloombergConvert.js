b434d947094982fd04d5f7bed306bebbb7f94199
c1b3rt00lk1 <c1b3rt00lk1t@protonmail.com>
2024-09-19 23:26:22 +0200

    Bloomberg convert

--- bloombergConvert.js
+++ bloombergConvert.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 
 
 // Starting from a hardcoded directory
-const directory = "/mnt/c/Retrospectiva/ZParaRenombrar/Bloomberg";
+const directory = "C:/Retrospectiva/ZParaRenombrar/Bloomberg";
 
 // const directory = process.argv[2];
 console.log(directory)
